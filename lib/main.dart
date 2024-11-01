e38c8587ffc62ba61d41a37b1f22652a1c3000d1
Montserrat-Ramos <ramoslopezmontserratviridiana@gmail.com>
2024-11-01 09:42:45 -0600

    Vertical app

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:junghanns/models/notification.dart';
 import 'package:junghanns/preferences/global_variables.dart';
@@ -63,6 +64,10 @@ void initWebSocket() {
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   await Firebase.initializeApp();
   FirebaseMessaging.onBackgroundMessage (messageHandler);
   FirebaseMessaging.onMessageOpenedApp.listen((message) {
